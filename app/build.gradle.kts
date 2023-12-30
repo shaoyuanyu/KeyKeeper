84498dad6e1fc670e44db14477963f17f4df7578
YSY <shaoyuanyu@foxmail.com>
2023-12-30 16:39:09 +0800

    v3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 31
         targetSdk = 34
         versionCode = 3
-        versionName = "3.0"
+        versionName = "3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
--- app/src/main/java/com/yusy/keykeeper/ui/pages/account/AppChooseScreen.kt
+++ app/src/main/java/com/yusy/keykeeper/ui/pages/account/AppChooseScreen.kt
@@ -19,9 +19,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.yusy.keykeeper.data.account.AppType
