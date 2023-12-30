b388cc4a3204387cc5e981c3e2b96f3dffe4f950
YSY <shaoyuanyu@foxmail.com>
2023-12-31 00:43:57 +0800

    v4.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.yusy.keykeeper"
         minSdk = 31
         targetSdk = 34
-        versionCode = 3
-        versionName = "3.1"
+        versionCode = 4
+        versionName = "4.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
