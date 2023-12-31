012e16a1fc0fbe4fabef5194e42623526411d1e0
YSY <shaoyuanyu@foxmail.com>
2023-12-31 23:28:40 +0800

    feat:app icon assets

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 31
         targetSdk = 34
         versionCode = 4
-        versionName = "4.0.0"
+        versionName = "4.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
